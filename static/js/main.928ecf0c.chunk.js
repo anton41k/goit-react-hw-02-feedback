fc881c102a2758d804f8e7730ea651d52a5abb95
anton41k <anton12petrenko@gmail.com>
2021-09-06 21:01:31 +0300

    Updates

--- static/js/main.928ecf0c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],[function(e,c){}],[[0,1]]]);
-//# sourceMappingURL=main.928ecf0c.chunk.js.map
